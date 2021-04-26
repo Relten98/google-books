cd4019bcfe4d68bea399d1568e3803cbb4b741f7
Relten98 <diezhandinoty@gmail.com>
2021-04-25 23:04:49 -0700

    Update booksController.js

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,5 +1,7 @@
+// Imports out models
 const db = require("../models");
 
+// This is a fancy thing
 module.exports = {
     findAll: function(req, res) {
       db.GoogleBooks
@@ -8,18 +10,24 @@ module.exports = {
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
+
+    // Finds one entry
     findOne: function(req, res) {
       db.GoogleBooks
         .findOne({id: req.params.id})
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
+
+    // SAves an entry
     save: function(req, res) {
       db.GoogleBooks
         .create(req.body)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
+
+    // Removes an entry
     remove: function(req, res) {
       db.GoogleBooks
         .findById({ _id: req.params.id })
