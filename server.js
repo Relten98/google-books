8df49ed5d495d71be2777cb47fe8a4c21f1b2569
Relten98 <diezhandinoty@gmail.com>
2021-04-26 17:26:22 -0700

    WORK DANG YOU

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const path = require("path");
 const routes = require("./routes");
+const mongoose = require("mongoose");
 
+// Express
 const app = express();
+
+// Port
 const PORT = process.env.PORT || 3001;
 
-
-// Define middleware here
+// Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
@@ -14,18 +17,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+// adding routes
 app.use(routes);
 
-// Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
-
-mongoose.connect(MONGODB_URI, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
+// request to the React app
+// define any API routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+//connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
+// start the API server
 app.listen(PORT, () => {
-  console.log(`API server on port ${PORT}!`);
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
-  
